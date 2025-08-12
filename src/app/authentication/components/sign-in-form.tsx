4a2c55b414dad83a40acb47cbb6fb7b3c27433e8
isac1708 <isac.figueredo@gmail.com>
2025-08-12 19:18:08 -0300

    feat: add UI for sign in and sign up form components

---
+++ src/app/authentication/components/sign-in-form.tsx
@@ -0,0 +1,94 @@
+"use cliente";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.email("Email inválido"),
+  password: z.string().min(6, "A Senha deve ter mais que 6 * caracteres"),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+const SigInForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: FormValues) {
+    console.log("Formulario enviado com sucesso!");
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Entrar</CardTitle>
+          <CardDescription>
+            Faça login na sua conta para continuar.
+          </CardDescription>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <CardContent className="grid gap-3">
+              <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>senha</FormLabel>
+                <FormControl>
+                  <Input placeholder="Digite sua senha:" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+            </CardContent>
+            <CardFooter>
+              <Button type="submit">Entrar</Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </>
+  );
+};
+
+export default SigInForm;
---
+++ src/app/authentication/components/sign-up-form.tsx
@@ -0,0 +1,130 @@
+"use cliente";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string("Nome inválido").trim().min(1, "Nome é obrigatório"),
+  email: z.email("Email inválido"),
+  password: z.string("senha inválida").min(6, "A Senha deve ter mais que 6 * caracteres"),
+  passwordConfirmation: z
+    .string()
+    .min(6, "A confirmação de senha deve ter mais que 6 caracteres"),
+}).refine((data) =>{return data.password === data.passwordConfirmation}, {
+  error: "As senhas não conferem",
+  path: ["passwordConfirmation"],
+},
+);
+
+type FormValues = z.infer<typeof formSchema>;
+const SignUpForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  });
+
+  function onSubmit(values: FormValues) {
+    console.log("Formulario enviado com sucesso!");
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Entrar</CardTitle>
+          <CardDescription>
+            Faça login na sua conta para continuar.
+          </CardDescription>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <CardContent className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu nome" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="senha" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite sua senha:" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="passwordConfirmation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirmação de senha</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Confirme sua senha" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button type="submit">Criar conta</Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </>
+  );
+};
+
+export default SignUpForm;
