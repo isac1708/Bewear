7124d4cc0a659a154d27a743f8349fecf3509014
isac1708 <isac.figueredo@gmail.com>
2025-09-28 20:04:26 -0300

    feat: add cart item

--- src/app/product-variant/[slug]/components/product-actions.tsx
+++ src/app/product-variant/[slug]/components/product-actions.tsx
@@ -41,7 +41,7 @@ const ProductActions = ({productVariantId}:ProductActionsProps) => {
         </div>
 
         <div className="flex flex-col space-y-4 px-5">
-          <AddToCartButton productVariantId={productVariantId} quantity={1} />
+          <AddToCartButton productVariantId={productVariantId} quantity={quantity} />
           <Button className="rounded-full" size="lg">
             Comprar Agora
           </Button>
