ec316067a73a701e4637ce8ca79f375a5e4802a8
isac1708 <isac.figueredo@gmail.com>
2025-12-15 18:46:01 -0300

    feat: add cart summary component

---
+++ src/app/cart/components/cart-summary.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image';
+import React from 'react'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { formatCentesToBRL } from '@/helpers/money';
+
+interface CartSummaryProps {
+    subtotalInCents: number;
+    totalInCents: number;
+    products:Array<{
+        id: string;
+        name: string;
+        variantName: string;
+        quantity: number;
+        priceInCents: number;
+        imageUrl: string;
+    }>;
+}
+
+const CartSummary = ({ subtotalInCents, totalInCents, products }: CartSummaryProps) => {
+    return (
+       <Card className="mt-5">
+        <CardHeader>
+            <CardTitle>Resumo do Pedido</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-3">
+        <div className="flex items-center justify-between">
+            <p className="text-sm">Subtotal</p>
+            <p className="text-sm text-muted-foreground font-medium">{formatCentesToBRL(subtotalInCents)}</p>
+        </div>
+        <div className="flex items-center justify-between">
+            <p className="text-sm">Frete</p>
+            <p className="text-sm text-muted-foreground font-medium">GRÁTIS</p>
+        </div>
+        <div className="flex items-center justify-between">
+            <p className="text-sm">Total</p>
+            <p className="text-sm text-muted-foreground font-medium">{formatCentesToBRL(totalInCents)}</p>
+        </div>
+        <div className="py3">
+            <Separator/>
+        </div>
+       {products.map(product=>( 
+        <div className="flex items-center justify-between" key={product.id} >
+      <div className="flex items-center gap-4">
+        <Image
+          src={product.imageUrl}
+          alt={product.variantName}
+          width={78}
+          height={78}
+          className="rounded-lg"
+        />
+        <div className="flex flex-col gap-1">
+          <p className="text-xs font-semibold">{product.name}</p>
+          <p className="text-muted-foreground text-xs font-medium">
+            {product.variantName}
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col items-end justify-center gap-2">
+        <p className="text-sm font-bold">
+          {formatCentesToBRL(product.priceInCents)}
+        </p>
+      </div>
+    </div>))}
+        </CardContent>
+       </Card>
+    )
+}
+
+export default CartSummary;
--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -2,11 +2,13 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import Footer from "@/app/authentication/components/common/footer";
 import Header from "@/app/authentication/components/common/header";
 import { db } from "@/db";
 import { cartTable, shippingAddressTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
+import CartSummary from "../components/cart-summary";
 import Addresses from "./components/addresses";
 
 const IdentificationPage = async () => {
@@ -19,7 +21,15 @@ const IdentificationPage = async () => {
   const cart = await db.query.cartTable.findFirst({
     where: eq(cartTable.userId, session.user.id),
     with: {
-      items: true,
+      items: {
+        with: {
+          productVariant: {
+            with: {
+              product: true,
+            },
+          },
+        },
+      },
     },
   });
   if (!cart || cart.items.length === 0) {
@@ -29,16 +39,34 @@ const IdentificationPage = async () => {
     where: eq(shippingAddressTable.userId, session.user.id),
   });
 
+
+  const CartTotalPriceInCents = cart.items.reduce(
+    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+    0,
+  );
   return (
-    <>
+    <div className="space-y-12">
       <Header />
-      <div className="px-5">
+      <div className="space-y-4 px-5">
         <Addresses
           shippingAddresses={shippingAddresses}
           defaultSelectedAddressId={cart.shippingAddressId}
         />
+        <CartSummary
+          subtotalInCents={CartTotalPriceInCents}
+          totalInCents={CartTotalPriceInCents}
+          products={cart.items.map((item) => ({
+            id: item.id,
+            name: item.productVariant.product?.name ?? item.productVariant.name,
+            variantName: item.productVariant.name,
+            quantity: item.quantity,
+            priceInCents: item.productVariant.priceInCents,
+            imageUrl: item.productVariant.imageUrl,
+          }))}
+        />
       </div>
-    </>
+      <Footer/>
+    </div>
   );
 };
 export default IdentificationPage;
